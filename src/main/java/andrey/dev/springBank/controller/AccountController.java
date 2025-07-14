a6a8da74da4dff2836cbf0bda75de0a20752c414
Maltsev Andrey <maltsevandreywork@gmail.com>
2025-07-14 23:30:57 +0300

    тоже самое

--- src/main/java/andrey/dev/springBank/controller/AccountController.java
+++ src/main/java/andrey/dev/springBank/controller/AccountController.java
@@ -1,14 +1,11 @@
 package andrey.dev.springBank.controller;
 
 
-import andrey.dev.springBank.config.ApplicationConfig;
 import andrey.dev.springBank.model.Account;
 import andrey.dev.springBank.services.Account.AccountService;
-import andrey.dev.springBank.services.Account.InMemoryAccountServiceImpl;
 import andrey.dev.springBank.services.TransferService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -16,31 +13,31 @@ import java.util.List;
 @RequestMapping("/api/v1/Accounts")
 @AllArgsConstructor
 public class AccountController {
-    private AccountService inMemoryAccountService;
+    private AccountService AccountService;
 
     @PostMapping("Create_Account")
     Account createAccount(@RequestBody Account account) {
-        return inMemoryAccountService.createAccount(account);
+        return AccountService.createAccount(account);
     }
 
     @PutMapping("Withdrawal")
     public Account withdrawalFromAccount(@RequestBody TransferService transferService) {
-        return inMemoryAccountService.withdrawalFromAccount(transferService.getSource(), transferService.getCount());
+        return AccountService.withdrawalFromAccount(transferService.getSource(), transferService.getCount());
     }
 
     @PutMapping("Replenishment")
     public Account replenishmentOnAccount(@RequestBody TransferService transferService) {
-        return inMemoryAccountService.replenishmentOnAccount(transferService.getSource(), transferService.getCount());
+        return AccountService.replenishmentOnAccount(transferService.getSource(), transferService.getCount());
     }
 
     @PutMapping("Transfer")
     public List<Account> transfer(@RequestBody TransferService transferService) {
-        return inMemoryAccountService.transfer(transferService.getSource(), transferService.getApply(), transferService.getCount());
+        return AccountService.transfer(transferService.getSource(), transferService.getApply(), transferService.getCount());
     }
 
     @DeleteMapping("Delete/{accountId}")
     void deleteAccount(@PathVariable int accountId) {
-        inMemoryAccountService.deleteAccount(accountId);
+        AccountService.deleteAccount(accountId);
     }
 
     ;
--- src/main/java/andrey/dev/springBank/controller/UserController.java
+++ src/main/java/andrey/dev/springBank/controller/UserController.java
@@ -2,8 +2,6 @@ package andrey.dev.springBank.controller;
 
 
 import andrey.dev.springBank.model.User;
-import andrey.dev.springBank.repositores.InMemoryUserRepository;
-import andrey.dev.springBank.services.User.InMemoryUserServiceImpl;
 import andrey.dev.springBank.services.User.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -14,25 +12,25 @@ import java.util.List;
 @RequestMapping("/api/v1/Users")
 @AllArgsConstructor
 public class UserController {
-    private UserService inMemoryUserService;
+    private UserService UserService;
 
     @PostMapping("createUser")
     public User createUser(@RequestBody User user) {
-        return inMemoryUserService.createUser(user);
+        return UserService.createUser(user);
     }
 
     @GetMapping("{userId}")
     public User findUserById(@PathVariable int userId) {
-        return inMemoryUserService.findUserById(userId);
+        return UserService.findUserById(userId);
     }
 
     @GetMapping
     public List<User> findAllUser() {
-        return inMemoryUserService.findAllUser();
+        return UserService.findAllUser();
     }
 
     @DeleteMapping("delete_User/{userId}")
     public void deleteById(@PathVariable int userId) {
-        inMemoryUserService.deleteUser(userId);
+        UserService.deleteUser(userId);
     }
 }
